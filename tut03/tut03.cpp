1ba10dc4153f6ac80c8de53237b0e65900737c2c
aritroCoder <ogocodinggod@gmail.com>
2023-09-04 13:21:06 +0530

    added TCP packets sniffing
    Signed-off-by: aritroCoder <ogocodinggod@gmail.com>

--- tut03/tut03.cpp
+++ tut03/tut03.cpp
@@ -37,7 +37,43 @@ void sniff_udp_packets(){
     }
 }
 
+void sniff_tcp_packets()
+{
+    try
+    {
+        // Specify the network interface to capture packets from (e.g., "eth0")
+        Sniffer sniffer("eth0");
+        vector<EthernetII> v;
+        // Start capturing packets
+        sniffer.sniff_loop([&](Packet &packet){
+            if(v.size()>=2) return false;
+            const IP &ip = packet.pdu()->rfind_pdu<IP>();
+            const TCP &tcp = packet.pdu()->rfind_pdu<TCP>();
+            const RawPDU &raw = packet.pdu()->rfind_pdu<RawPDU>();
+            // add the packet to vector and write to pcap file
+            v.push_back(EthernetII() / ip / tcp / raw);
+            auto new_pkt = EthernetII() / ip / tcp / raw;
+            // swap the source and destination IP address
+            new_pkt.rfind_pdu<IP>().src_addr(ip.dst_addr());
+            new_pkt.rfind_pdu<IP>().dst_addr(ip.src_addr());
+            // add the packet to vector and write to pcap file
+            v.push_back(new_pkt);
+            // send the packet
+            PacketSender sender;
+            sender.send(new_pkt, "eth0");
+            return true;
+        });
+        PacketWriter writer("./output/TCP_Packets_2101AI40.pcap", DataLinkType<EthernetII>());
+        writer.write(v.begin(), v.end());
+    }
+    catch (std::exception &ex)
+    {
+        std::cerr << "Error: " << ex.what() << std::endl;
+    }
+}
+
 int main(){
     sniff_udp_packets();
+    sniff_tcp_packets();
     return 0;
 }
